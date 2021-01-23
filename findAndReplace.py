66792dff34b99823c23f602bf46ade7827e9f3e1
AsharGit <32973667+AsharGit@users.noreply.github.com>
2021-01-22 22:48:14 -0600

    New update
    reverseString and findAndReplace completed.

---
+++ findAndReplace.py
@@ -0,0 +1,8 @@
+# Get input sentence from user
+astring = input('Please enter a sentence: ')
+
+# Replace any instance of the word 'python' with 'pythons'
+bstring = astring.replace('python', 'pythons')
+
+# Print new string
+print(bstring)
--- reverseString.py
+++ reverseString.py
@@ -1,7 +1,15 @@
 # Get input string from user
 astring = input('Please enter string to modify and reverse: ')
+
+# Get length of string
 length = len(astring)
+
+# Check if the string is at least 3 letters long
 if length >= 3:
+    # remove the first and last letter
+    bstring = astring[1:length-1]
+else:
+    bstring = astring
 
 # Print the reverse of the string
-print(astring[::-1])
+print(bstring[::-1])
