91e0447659a2ada3d869e8b389c09c59d3a0593d
AsharGit <32973667+AsharGit@users.noreply.github.com>
2021-03-06 11:26:36 -0600

    ICP 7

--- Source/ICP-7/natlang_processing.py
+++ Source/ICP-7/natlang_processing.py
@@ -5,54 +5,60 @@ from nltk.stem import PorterStemmer, LancasterStemmer, SnowballStemmer, WordNetL
 from nltk import trigrams
 from nltk import wordpunct_tokenize, ne_chunk, pos_tag
 
-
+# Get URL and parse the page
 url = requests.get('https://en.wikipedia.org/wiki/Google')
 page = BeautifulSoup(url.content, 'html.parser')
-
-
-file = open('index.txt', 'w', encoding="utf-8")
-file.write(page.body.text)
-file.close()
-
 text = page.body.text
 
-# # Tokenization with words and sentences
-# wtokens = nltk.word_tokenize(text)
-# stokens = nltk.sent_tokenize(text)
-#
-# # Word tokenization
-# for t in wtokens:
-#     print(t)
-# # Sentence tokenization
-# for s in stokens:
-#     print(s)
+# Open file and write body of text
+file = open('index.txt', 'w', encoding="utf-8")
+file.write(text)
+file.close()
 
-# # POS tagging
-# posText = nltk.word_tokenize(text)
-# print(nltk.pos_tag(posText))
-#
-# # Stemming
-# print('Porter Stemming: ')
-# pStemmer = PorterStemmer()
-# print(pStemmer.stem(text))
-#
-# print('Lancaster Stemming: ')
-# lStemmer = LancasterStemmer()
-# print(lStemmer.stem(text))
-#
-# print('Snowball Stemming: ')
-# sStemmer = SnowballStemmer('english')
-# print(sStemmer.stem(text))
+
+# Tokenization with words and sentences
+wtokens = nltk.word_tokenize(text)
+stokens = nltk.sent_tokenize(text)
+
+# Word tokenization
+print('Word Tokenization: ')
+for t in wtokens:
+    print(t)
+
+# Sentence tokenization
+print('Sentence tokenization: ')
+for s in stokens:
+    print(s)
+
+# POS tagging
+print('Part Of Speech tagging: ')
+posText = nltk.word_tokenize(text)
+print(nltk.pos_tag(posText))
+
+# Stemming
+print('Porter Stemming: ')
+pStemmer = PorterStemmer()
+print(pStemmer.stem(text))
+
+print('Lancaster Stemming: ')
+lStemmer = LancasterStemmer()
+print(lStemmer.stem(text))
+
+print('Snowball Stemming: ')
+sStemmer = SnowballStemmer('english')
+print(sStemmer.stem(text))
 
 # Lemmatization
 lemmatizer = WordNetLemmatizer()
 print(lemmatizer.lemmatize(text))
 
 # Trigram
+print('Trigram: ')
 wtokens = nltk.word_tokenize(text)
 triword = trigrams(wtokens)
 for g in triword:
     print(g)
 
 # NER
+print('Named Entity Recognition: ')
 print(ne_chunk(pos_tag(wordpunct_tokenize(text))))
